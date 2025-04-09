95573a2ddb84fa0ed7bcfa0f6aa215ba2ad67a72
siog <siog@LAPTOP-545TJ6PE>
2025-04-09 15:41:58 +0100

    Add comments for exercise question, Instance Variables and a Constructor

--- ArraysExercises/src/ie/atu/sw/PrimeParser.java
+++ ArraysExercises/src/ie/atu/sw/PrimeParser.java
@@ -1,6 +1,21 @@
 package ie.atu.sw;
 
+/*
+ * Write a programme called PrimeParser that reads in 
+ * a set of numbers into the String[] args array of main() 
+ * and then converts them to an integer array. The integer 
+ * array should discard any number that is not prime. 
+ * A number is prime if its only divisors are itself and 1. 
+ * One (naive, but okay for this exercise) way to test if a 
+ * number is prime is to check if every integer between 1 and 
+ * itself (exclusive) divides evenly.
+ */
+
 public class PrimeParser {
+	
+	//INSTANCE VARIABLES
+	
+	//CONSTRUCTOR
 	
 	public static void main(String[] args) {
 		
